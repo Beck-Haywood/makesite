998c01d450dcdb56127fb1f575872cbb0f24341a
Beck-Haywood <beck.haywood@students.makeschool.com>
2021-02-04 19:03:43 -0900

    finished mvp

--- makesite.go
+++ makesite.go
@@ -2,34 +2,50 @@ package main
 
 import (
 	// "fmt"
-	"bytes"
+	"flag"
+	"os"
+	// "bytes"
 	"html/template"
 	"io/ioutil"
+
 )
 
+type Post struct {
+	Text string
+}
+
 func main() {
-	// Reads file
-	FileContents, err := ioutil.ReadFile("first-post.txt")
+	// Get flag value
+	filePath := flag.String("postPath", "first-post.txt", "Name of file you want to read from.")
+	outputPath := flag.String("outputPath", "new-file.html", "Name of file you want to output to.")
+	flag.Parse()
+
+	// Read file using flag 
+	fileContents, err := ioutil.ReadFile(*filePath)
 	if err != nil {
 		panic(err)
 	}
-	// define b as bytes, made to convert string into usable bytes to write to file
-	var b bytes.Buffer
-	// Set template
+	
+	post := Post{string(fileContents)}
+
+	// Init template
 	t := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
-	err = t.Execute(&b, string(FileContents))
 
-	if err != nil {
-		panic(err)
-	}
-	// Write to file
-	err = ioutil.WriteFile("new-file.html", b.Bytes(), 777)
-
-	if err != nil {
-		panic(err) // This will fail if the file exists
+	// Create output file if it does not exist
+	file, err := os.Create(*outputPath)
+	// This will fail if the file ("new-file.html") exists
 		// Because it tries to write to a file that already exists
 		// Solution: Store things inside of a folder that the user running the build has permissions to.
 		// Link to solution: https://support.circleci.com/hc/en-us/articles/360003649774-Permission-Denied-When-Creating-Directory-or-Writing-a-File
+	if err != nil{
+		panic(err)
 	}
+
+	errExecute := t.Execute(file, post)
+	if errExecute != nil {
+		panic(errExecute)
+	}
+
+	file.Close()
 
 }
