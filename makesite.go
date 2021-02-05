c2b3ff9c1a889d28c08f7c312a0b77c3dfff9c4a
Beck-Haywood <beck.haywood@students.makeschool.com>
2021-02-04 19:00:20 -0900

    reads file, uses template to create html file

--- makesite.go
+++ makesite.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	// "fmt"
+	"bytes"
+	"html/template"
+	"io/ioutil"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	// Reads file
+	FileContents, err := ioutil.ReadFile("first-post.txt")
+	if err != nil {
+		panic(err)
+	}
+	// define b as bytes, made to convert string into usable bytes to write to file
+	var b bytes.Buffer
+	// Set template
+	t := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
+	err = t.Execute(&b, string(FileContents))
+
+	if err != nil {
+		panic(err)
+	}
+	// Write to file
+	err = ioutil.WriteFile("new-file.html", b.Bytes(), 777)
+
+	if err != nil {
+		panic(err) // This will fail if the file exists
+		// Because it tries to write to a file that already exists
+		// Solution: Store things inside of a folder that the user running the build has permissions to.
+		// Link to solution: https://support.circleci.com/hc/en-us/articles/360003649774-Permission-Denied-When-Creating-Directory-or-Writing-a-File
+	}
+
 }
